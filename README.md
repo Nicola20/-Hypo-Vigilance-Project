4a06306de8bf82bfeaa189c942c4c05d267164a3
xiqe4652 <nicola.lea.libera@uni-weimar.de>
2021-12-14 00:45:04 +0100

    basic setup

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Hypo-Vigilance-Project
+
+### Setup
+
+- Install pygame with `pip3 install pygame`. If you want to use a virtual environment, use `python3 -m venv venv` then
+activate the environment with `source venv/bin/activate`. Now you can install pygame with 
+`pip3 install -r requirements.txt`. 
+- To start the game run `python3 space_game.py`.
---
+++ src/space_game.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env/python3
+
+"""
+------------------------------------------------------------------------------------------------
+ @authors       Nicola Lea Libera (117073), Laura Simon (), Chiranjeevi Janjanam()
+------------------------------------------------------------------------------------------------
+ Description: This is the implementation of a little space game that detects if a player is
+              pressing the buttons too hard and punishes him/her if this is the case.
+              The goal of the game is to help train people to stay calm even in stressful
+              situations.
+------------------------------------------------------------------------------------------------
+"""
+
+import pygame
+import random
+
+# colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+PLAYING = True
+BACKGROUND_SPEED = 5
+
+
+# initialize game
+pygame.init()
+
+# display a window
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+screen.fill(BLACK)
+
+while PLAYING:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            PLAYING = False
+            pygame.quit()
