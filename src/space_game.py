34c38f7014cb797a5bd98cf5139ab6ae5c310238
xiqe4652 <nicola.lea.libera@uni-weimar.de>
2021-12-14 15:57:32 +0100

    moving star background finished

--- src/space_game.py
+++ src/space_game.py
@@ -18,7 +18,8 @@ import random
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 PLAYING = True
-BACKGROUND_SPEED = 5
+BACKGROUND_SPEED = 2
+FPS = 60
 
 
 # initialize game
@@ -26,10 +27,55 @@ pygame.init()
 
 # display a window
 screen = pygame.display.set_mode((0, 0))
+clock = pygame.time.Clock()
 screen.fill(BLACK)
+
+# logic for moving stars in background
+width, height = pygame.display.get_surface().get_size()
+
+
+class Star(object):
+    def __init__(self, x, y, speed):
+        self.radius = 1
+        self.x = x
+        self.y = y
+        self.speed = speed
+
+    def draw(self):
+        pygame.draw.circle(screen, WHITE, (self.x, self.y), self.radius)
+
+    def move(self):
+        self.y += self.speed
+
+    def appear_as_new_star(self):
+        if self.y >= height:
+            self.y = 0
+            self.x = random.randint(1, width - 1)
+
+
+stars = []
+
+for i in range(200):
+    x_pos = random.randint(1, width - 1)
+    y_pos = random.randint(1, height - 1)
+    stars.append(Star(x_pos, y_pos, BACKGROUND_SPEED))
+
 
 while PLAYING:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             PLAYING = False
-            pygame.quit()
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            PLAYING = False
+
+    screen.fill(BLACK)
+
+    for star in stars:
+        star.draw()
+        star.move()
+        star.appear_as_new_star()
+
+    pygame.display.flip()
+    clock.tick(FPS)
+
+pygame.quit()
